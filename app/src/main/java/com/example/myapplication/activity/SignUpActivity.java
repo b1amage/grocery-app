1ddfc1871496dba4dbd8b0cd4729609e1b4065bd
Bacenz <bentheuchiha@gmail.com>
2022-12-29 19:18:27 +0700

    finalize signin, add signup

---
+++ app/src/main/java/com/example/myapplication/activity/SignUpActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    //TODO: connect between signup and signin using intent
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
