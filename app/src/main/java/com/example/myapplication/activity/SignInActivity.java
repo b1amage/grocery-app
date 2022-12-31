449209e13972ada5be5daf5c57aa2c5eabc88750
Bacenz <bentheuchiha@gmail.com>
2022-12-31 19:48:53 +0700

    add signin stuff

--- app/src/main/java/com/example/myapplication/activity/SignInActivity.java
+++ app/src/main/java/com/example/myapplication/activity/SignInActivity.java
@@ -3,10 +3,19 @@ package com.example.myapplication.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText editText_email,editText_password;
+    private Button button_signIn;
+    private TextView text_forgot, text_signUp;
+    private CheckBox checkBox_remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
