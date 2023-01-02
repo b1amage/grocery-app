a8e55a89cdf3c5f6a525cd94a76e4d9c31d94832
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2023-01-02 12:15:39 +0700

    feat: cart ui

--- app/src/main/java/com/example/myapplication/adapter/ItemAdapter.java
+++ app/src/main/java/com/example/myapplication/adapter/ItemAdapter.java
@@ -14,10 +14,18 @@ import java.util.List;
 public class ItemAdapter extends BaseAdapter {
 
     private List<Item> items;
+    private boolean isInCart;
 
     public ItemAdapter(List<Item> items) {
         this.items = items;
+        this.isInCart = false;
     }
+
+    public ItemAdapter(List<Item> items, boolean isInCart) {
+        this.items = items;
+        this.isInCart = isInCart;
+    }
+
 
     @Override
     public int getCount() {
@@ -38,15 +46,24 @@ public class ItemAdapter extends BaseAdapter {
     public View getView(int i, View convertView, ViewGroup parent) {
         View itemView;
         if (convertView == null) {
-            itemView = View.inflate(parent.getContext(), R.layout.item, null);
+            itemView = View.inflate(parent.getContext(), isInCart ? R.layout.cart_item : R.layout.item, null);
         } else itemView = convertView;
 
         Item item = (Item) getItem(i);
 
-        ((ImageView) itemView.findViewById(R.id.item_img)).setImageResource(item.getImg());
-        ((TextView) itemView.findViewById(R.id.item_name)).setText(item.getName());
-        ((TextView) itemView.findViewById(R.id.item_category)).setText(item.getCategory());
-        ((TextView) itemView.findViewById(R.id.item_price)).setText("$" + String.valueOf(item.getPrice()));
+
+        if (!isInCart) {
+            ((ImageView) itemView.findViewById(R.id.item_img)).setImageResource(item.getImg());
+            ((TextView) itemView.findViewById(R.id.item_name)).setText(item.getName());
+            ((TextView) itemView.findViewById(R.id.item_category)).setText(item.getCategory());
+            ((TextView) itemView.findViewById(R.id.item_price)).setText("$" + String.valueOf(item.getPrice()));
+        } else {
+            ((ImageView) itemView.findViewById(R.id.cart_item_img)).setImageResource(item.getImg());
+            ((TextView) itemView.findViewById(R.id.cart_item_name)).setText(item.getName());
+            ((TextView) itemView.findViewById(R.id.cart_item_category)).setText(item.getCategory());
+            ((TextView) itemView.findViewById(R.id.cart_item_price)).setText("$" + String.valueOf(item.getPrice()));
+        }
+
 
 
         return itemView;
