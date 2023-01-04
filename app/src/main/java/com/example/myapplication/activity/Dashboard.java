323b77d666fa27873b8735ef979557a0bbed766a
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-01-04 13:24:06 +0700

    fix: remove redundant folder

--- app/src/main/java/com/example/myapplication/activity/Dashboard.java
+++ app/src/main/java/com/example/myapplication/activity/Dashboard.java
@@ -1,18 +1,21 @@
 package com.example.myapplication.activity;
 
+import com.example.myapplication.adapter.CategoryItemAdapter;
 import com.example.myapplication.components.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.GridView;
+import android.widget.ListView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.adapter.CategoryAdapter;
 import com.example.myapplication.content.Categories;
+import com.example.myapplication.content.Items;
 
 public class Dashboard extends AppCompatActivity {
-    private GridView categoryGridView;
-    private Categories categories = new Categories();
+    private ListView categoryGridView;
+    private Items items = new Items();
     private ActionBar actionBar = new ActionBar(R.id.actionBar, this);
 
     @Override
@@ -26,7 +29,7 @@ public class Dashboard extends AppCompatActivity {
 
         actionBar.createActionBar("Dashboard", R.drawable.logo_icon, 0);
         categoryGridView = findViewById(R.id.categoryList);
-        CategoryAdapter categoryAdapter = new CategoryAdapter(this, categories.getCategories());
+        CategoryItemAdapter categoryAdapter = new CategoryItemAdapter(this, items.getItems());
         categoryGridView.setAdapter(categoryAdapter);
     }
 }
