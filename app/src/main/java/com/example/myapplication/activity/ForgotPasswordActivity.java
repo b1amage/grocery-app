8027f9729001301abf5b2bac60ea0f1964655a7b
Bacenz <bentheuchiha@gmail.com>
2023-01-08 16:01:46 +0700

    add some OTP forgot activity

--- app/src/main/java/com/example/myapplication/activity/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/myapplication/activity/ForgotPasswordActivity.java
@@ -3,11 +3,16 @@ package com.example.myapplication.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.myapplication.R;
-import com.example.myapplication.utilities.Button;
+import com.example.myapplication.api.APIHandler;
+import com.example.myapplication.api.VolleyResponseListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ForgotPasswordActivity extends BaseActivity {
 
@@ -20,12 +25,37 @@ public class ForgotPasswordActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
+        button_next = (Button) findViewById(R.id.button_next);
+        button_goback = (ImageButton) findViewById(R.id.button_goback);
+        edittext_email = (EditText) findViewById(R.id.edittext_email);
+
         button_goback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ForgotPasswordActivity.this, SignInActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        button_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JSONObject jsonObject = new JSONObject();
+                (new APIHandler(ForgotPasswordActivity.this)).postRequest(jsonObject, "/auth/register", new VolleyResponseListener() {
+                    @Override
+                    public void onError(String message, int statusCode) {
+                        edittext_email.setError(message);
+                    }
+                    @Override
+                    public void onResponse(JSONObject response) throws JSONException {
+                        jsonObject.put("hash", response.get("hash"));
+                        Intent intent = new Intent(ForgotPasswordActivity.this, OTPActivity.class);
+                        intent.putExtra("user",jsonObject.toString());
+                        startActivity(intent);
+                        finish();
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/example/myapplication/activity/OTPForgotActivity.java
+++ app/src/main/java/com/example/myapplication/activity/OTPForgotActivity.java
@@ -1,26 +1,122 @@
 package com.example.myapplication.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.api.APIHandler;
+import com.example.myapplication.api.VolleyResponseListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class OTPForgotActivity extends BaseActivity {
 
-    private EditText code1, code2, code3, code4;
+    private EditText code1, code2, code3, code4, code5, code6;
+    private Button button_next;
+    private TextView text_receive, text_resend, text_second;
+    private JSONObject jsonObject;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+        try {
+            jsonObject = new JSONObject(getIntent().getStringExtra("user"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         code1 = findViewById(R.id.code1);
         code2 = findViewById(R.id.code2);
         code3 = findViewById(R.id.code3);
         code4 = findViewById(R.id.code4);
+        code5 = findViewById(R.id.code5);
+        code6 = findViewById(R.id.code6);
+        button_next = (Button) findViewById(R.id.button_next);
+        text_receive = (TextView) findViewById(R.id.text_receive);
+        text_resend = (TextView) findViewById(R.id.text_resend);
+        text_second = (TextView) findViewById(R.id.text_second);
 
         setUpOTP();
+        reverseTimer();
+        button_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = code1.getText().toString() +
+                        code2.getText().toString() +
+                        code3.getText().toString() +
+                        code4.getText().toString() +
+                        code5.getText().toString() +
+                        code6.getText().toString();
+                try {
+                    jsonObject.put("otp",code);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                (new APIHandler(OTPForgotActivity.this)).postRequest(jsonObject, "/auth/verify-otp-register", new VolleyResponseListener() {
+                    @Override
+                    public void onError(String message, int statusCode) {
+                        code6.setError(message);
+                    }
+                    @Override
+                    public void onResponse(JSONObject response) throws JSONException {
+                        Intent intent = new Intent(OTPForgotActivity.this, RegisterFinishedActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+            }
+        });
     }
+
+    public void reverseTimer(){
+        new CountDownTimer(90000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                text_second.setText(millisUntilFinished / 1000 + " seconds" );
+                // logic to set the EditText could go here
+            }
+
+            public void onFinish() {
+                text_second.setText("Please resend the code");
+                text_resend.setText("Resend here!");
+                text_resend.setTextColor(getResources().getColor(R.color.primary_100));
+                text_resend.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        (new APIHandler(OTPForgotActivity.this)).postRequest(jsonObject, "/auth/register", new VolleyResponseListener() {
+                            @Override
+                            public void onError(String message, int statusCode) {
+                            }
+                            @Override
+                            public void onResponse(JSONObject response) throws JSONException {
+                                reverseTimer();
+                                text_resend.setText("Please wait!");
+                                text_resend.setTextColor(getResources().getColor(R.color.tertiary_gray));
+                                text_resend.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+
+        }.start();
+    }
+
 
     private void setUpOTP(){
         code1.addTextChangedListener(new TextWatcher() {
@@ -69,6 +165,42 @@ public class OTPForgotActivity extends BaseActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if(!charSequence.toString().trim().isEmpty()){
                     code4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        code4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty()){
+                    code5.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        code5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!charSequence.toString().trim().isEmpty()){
+                    code6.requestFocus();
                 }
             }
 
