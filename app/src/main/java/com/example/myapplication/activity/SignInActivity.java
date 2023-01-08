70e737a9126207aba579e8730a286aaeb79b456a
Bacenz <bentheuchiha@gmail.com>
2023-01-08 15:08:42 +0700

    signin

--- app/src/main/java/com/example/myapplication/activity/SignInActivity.java
+++ app/src/main/java/com/example/myapplication/activity/SignInActivity.java
@@ -2,17 +2,29 @@ package com.example.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.api.APIHandler;
+import com.example.myapplication.api.VolleyResponseListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignInActivity extends AppCompatActivity {
 
-    private EditText editText_email,editText_password;
+    private EditText editText_email;
+    private TextInputEditText editText_password;
     private Button button_signIn;
     private TextView text_forgot, text_signUp;
     private CheckBox checkBox_remember;
@@ -21,5 +33,59 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        editText_email = (EditText) findViewById(R.id.edittext_email);
+        editText_password = (TextInputEditText) findViewById(R.id.edittext_password);
+        button_signIn = (Button) findViewById(R.id.button_signin);
+        text_forgot = (TextView) findViewById(R.id.text_forgot);
+        text_signUp = (TextView) findViewById(R.id.text_signup);
+
+        TextInputLayout password = (TextInputLayout) findViewById(R.id.password);
+        editText_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                password.setEndIconVisible(true);
+            }
+        });
+
+        button_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("email",editText_email.getText().toString());
+                    jsonObject.put("password",editText_password.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                (new APIHandler(SignInActivity.this)).postRequest(jsonObject, "/auth/login", new VolleyResponseListener() {
+                    @Override
+                    public void onError(String message, int statusCode) {
+                        if(message.contains("email")){
+                            editText_email.setError(message);
+                        } else if(message.contains("password")){
+                            editText_password.setError(message);
+                            password.setEndIconVisible(false);
+                        } else {
+                            System.out.println(message + statusCode);
+                        }
+                    }
+                    @Override
+                    public void onResponse(JSONObject response) throws JSONException {
+                        System.out.println(response.toString());
+                    }
+                });
+
+            }
+        });
+
+        text_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
